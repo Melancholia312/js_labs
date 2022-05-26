4a4dde9603cf923167e2bd6801f4405b5b1edcf3
Melancholia312 <67428510+Melancholia312@users.noreply.github.com>
2022-05-27 02:19:24 +0300

    Add files via upload

--- lab_5/js/animation.js
+++ lab_5/js/animation.js
@@ -1,7 +1,23 @@
 var blocks = document.getElementsByClassName("block");
 var button = document.getElementById("toggleThemeBtn");
+var buttonAnimate = document.getElementById("hideBall");
+var circle = document.getElementsByClassName("circle")[0];
 
 for (let i=0; i<blocks.length; i++){
     blocks[i].classList.add("animate");
 }
 button.classList.add("animate");
+buttonAnimate.classList.add("animate");
+circle.classList.add("animate");
+
+function hide() {
+    var ball = document.getElementsByClassName("ball")[0];
+    if (ball.style.visibility == "hidden") {
+        ball.style.visibility = "visible";
+        buttonAnimate.innerText = "Убрать шарик";
+    } else {
+        ball.style.visibility = "hidden";
+        buttonAnimate.innerText = "Показать шарик";
+    }
+
+}
